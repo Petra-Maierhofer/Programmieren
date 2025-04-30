430e763a67a18934b7b624f1dc769af24947879b
Petra-Maierhofer <p.maierhofer@students.htl-leonding.ac.at>
2025-04-30 14:59:57 +0200

    Variabl

---
+++ School/2_Variablen/02_01_Visitenkarte/Program.cs
@@ -0,0 +1,22 @@
+﻿/*--------------------------------------------------------------
+ *			Visitenkarte
+ *--------------------------------------------------------------
+*/
+
+using System;
+
+string name;
+string livingPlace;
+
+Console.Write("What is your name: ");
+name = Console.ReadLine();
+
+Console.Write("What is your living place: ");
+livingPlace = Console.ReadLine();
+
+Console.WriteLine("*****************************");
+Console.WriteLine($"* {name, -25} *");
+Console.WriteLine("*****************************");
+Console.WriteLine($"* {livingPlace,-25} *");
+Console.WriteLine("*****************************");
+
---
+++ School/2_Variablen/02_02_WordShuffle/Program.cs
@@ -0,0 +1,41 @@
+﻿/*--------------------------------------------------------------
+ *			Hallo Welt
+ *--------------------------------------------------------------
+*/
+
+using System;
+
+string word1;
+string word2;
+string word3;
+
+Console.WriteLine("WordShuffle");
+Console.WriteLine("===========");
+Console.WriteLine();
+
+Console.Write("Please enter 1st word: ");
+word1 = Console.ReadLine();
+
+Console.Write("Please enter 2nd word: ");
+word2 = Console.ReadLine();
+
+Console.Write("Please enter 3rd word: ");
+word3 = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("Shuffling words ... six possible combinations: ");
+Console.WriteLine();
+
+Console.WriteLine($"{word1} {word2} {word3}");
+Console.WriteLine($"{word1} {word3} {word2}");
+Console.WriteLine($"{word2} {word3} {word1}");
+Console.WriteLine($"{word2} {word1} {word3}");
+Console.WriteLine($"{word3} {word1} {word2}");
+Console.WriteLine($"{word3} {word2} {word1}");
+
+Console.WriteLine();
+
+Console.Write("Press any key to finish ...");
+Console.ReadLine();
+
+
